c093263fc27e1ceb66bf6cab9f6f78d75bffccc7
kalebert <evanschulte9@gmail.com>
2020-10-14 22:20:48 -0600

    The Topics View

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page the shows all topics
+    path('topics/', views.topics, name='topics'),
 ]

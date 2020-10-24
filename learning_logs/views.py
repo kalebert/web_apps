83420e955c50e7042cda23e2f08810c6e8f0bccf
kalebert <evanschulte9@gmail.com>
2020-10-23 18:59:02 -0600

    Restricting Access Throughout Learning Log

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -17,6 +17,7 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id): 
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
@@ -24,6 +25,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -40,6 +42,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -61,6 +64,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit and existing entry."""
     entry = Entry.objects.get(id=entry_id)

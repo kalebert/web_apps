f14213366f4f12dafbbb27cb7c23190191f2677e
kalebert <evanschulte9@gmail.com>
2020-11-11 15:05:46 -0600

    Finalize Error Page 404 and 500

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v1ux$z%!^bvat1+sv%4jaovao&o!qqq&c#b2ho)a!33h44192!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -64,7 +64,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
